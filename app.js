44bcb3c033734e4a88f16b621f0280698fa385f1
Isaac <isaacschoolacc@gmail.com>
2019-11-19 15:41:11 -0800

    added local storage method to constructor

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ var currentUserChoice = '';
 
 var winCountingArray = [0, 0]; // [0] is currentUser , [1] is badGuy
 
+var scoreObj;
+
 var turnCounter = 0;
 
 var userHistory = []; // LS later
 
 var currentUser = {
   winLossHistory: [0, 0],
-  userHistory : 0
+  userHistory: 0
 }
 
 // Local storage into user vvv
@@ -22,23 +24,13 @@ var User = function (name) {
   this.winLossHistory = [0, 0];
   this.userHistory = 0;
 
-  this.render = function (domReference) {
-
-    // var score = document.getElementById('score');
-    var tableContents = document.getElementById('scores');
-    var tr = document.createElement('tr');
-    var nameCell = document.createElement('td');
-    nameCell.textContent = this.name;
-    tr.append(nameCell);
-
-    var matchCount = document.createElement('td');
-    matchCount.textContent = this.userHistory;
-    tr.append(matchCount);
-  };
+  this.saveToLocal = function(){
+    var scoreData = JSON.stringify(this);
+    localStorage.setItem('SCORE_DATA', scoreData);
+  }
 }
 
 
-}
 
 // Evaluate Choices
 // sword beats spell, spell beats shield, shield beats sword
@@ -49,11 +41,7 @@ var shieldTarget = document.getElementById('shieldTarget');
 
 
 
-  // Action Listeners with What Happens when Clicked
-
-swordTarget.addEventListener('click', event => {
-  currentUserChoice = 'sword';
-  battleFunction();
+// Action Listeners with What Happens when Clicked
 
 
 swordTarget.addEventListener('click', event => {
@@ -85,7 +73,7 @@ function battleFunction() {
   } else if ((currentUserChoice === 'sword' && badGuyChoice === 'spell') ||
     (currentUserChoice === 'spell' && badGuyChoice === 'shield') ||
     (currentUserChoice === 'shield' && badGuyChoice === 'sword')) {
-      //user win condition
+    //user win condition
     winCountingArray[0]++;
     alert(`good point ${winCountingArray[0]}\n\ngood ${currentUserChoice}| bad ${badGuyChoice}`);
   } else {
@@ -111,209 +99,209 @@ function battleFunction() {
       winCountingArray = [0, 0];
     } else {
       var newUser = prompt('INPUT NAME: ');
-      var scoreObj = new User(newUser);
+      scoreObj = new User(newUser); //data constructed to obj
       scoreObj.userHistory = currentUser.userHistory;
       scoreObj.winLossHistory = currentUser.winLossHistory;
-      scoreObj.render();
+      scoreObj.saveToLocal();
     }
   }
+};
+
+// Current Scoreboard NOT HIGH SCORE
+
+function scoreBoard() {
+  var scoreCardReference = document.getElementById('scoreCard');
+  console.log(scoreCardReference);
+  var pElement = document.createElement('p');
+  pElement.textContent = 'This is a test';
+  scoreCardReference.append(pElement);
 }
 
-  // Current Scoreboard NOT HIGH SCORE
 
-  function scoreBoard() {
-    var scoreCardReference = document.getElementById('scoreCard');
-    console.log(scoreCardReference);
-    var pElement = document.createElement('p');
-    pElement.textContent = 'This is a test';
-    scoreCardReference.append(pElement);
-  }
+scoreBoard();
 
+// ====================================================================================
 
-  scoreBoard();
+//Isaacs animation code - You guys can work above this
 
-  // ====================================================================================
-
-  //Isaacs animation code - You guys can work above this
-
-  // to do list
-  // - be able to change the animation as the result of an 
-  // if statement and then have it switch back to idle after completion of animation.
+// to do list
+// - be able to change the animation as the result of an 
+// if statement and then have it switch back to idle after completion of animation.
 
 
-  // Thoughts about animation
-  // - idle animation 2 frames back and forth.
-  // - battle scenes are lots of frames iterated over once!
-  // - while battle scenes run, idle must disappear.
-  // - when battle ends units return to idle.
-  // - walk back to idle spots in battle scene.
+// Thoughts about animation
+// - idle animation 2 frames back and forth.
+// - battle scenes are lots of frames iterated over once!
+// - while battle scenes run, idle must disappear.
+// - when battle ends units return to idle.
+// - walk back to idle spots in battle scene.
 
 
-  var SPRITE_SIZE = 32;
+var SPRITE_SIZE = 32;
 
 
-  var canvas = document.getElementById('gameScreen');
-  var ctx = canvas.getContext('2d');
+var canvas = document.getElementById('gameScreen');
+var ctx = canvas.getContext('2d');
 
 
 
-  //preloading all images
-  // var cloudImage = new Image();
-  // cloudImage.src = "images/cloud1.png";
+//preloading all images
+// var cloudImage = new Image();
+// cloudImage.src = "images/cloud1.png";
 
-  // var cloudImage2 = new Image();
-  // cloudImage2.src = "images/cloud2.png";
+// var cloudImage2 = new Image();
+// cloudImage2.src = "images/cloud2.png";
 
-  // var cloudImage3 = new Image();
-  // cloudImage3.src = "images/cloud3.png";
+// var cloudImage3 = new Image();
+// cloudImage3.src = "images/cloud3.png";
 
-  var backgroundImg = new Image();
-  backgroundImg.src = "images/BG.png";
+var backgroundImg = new Image();
+backgroundImg.src = "images/BG.png";
 
-  swordTarget.src = "images/swordSprite.png";
-  spellTarget.src = "images/spellSprite.png";
-  shieldTarget.src = "images/shieldSprite.png";
-
-
-  var CharAnimation = function (frameSet) {
-    this.count = 0,
-      this.delay = 20,
-      this.frame = 0,
-      this.frameSet = frameSet,
-      this.frameIndex = 0,
-
-      //for an animation use the change function to change to it and change the frameset to an array of animation. the animation may also need to be its own object.
-      this.change = function (frameSet, delay) {
-        if (this.frameSet != frameSet) {
-
-          this.count = 0;
-          this.delay = delay;
-          this.frameIndex = 0;
-          this.frameSet = frameSet;
-          this.frame = frameSet[this.frameIndex];
-        }
-      },
-
-      this.update = function () {
-        this.count++;
-
-        if (this.count >= this.delay) {
-          this.count = 0;
+swordTarget.src = "images/swordSprite.png";
+spellTarget.src = "images/spellSprite.png";
+shieldTarget.src = "images/shieldSprite.png";
 
 
+var CharAnimation = function (frameSet) {
+  this.count = 0,
+    this.delay = 20,
+    this.frame = 0,
+    this.frameSet = frameSet,
+    this.frameIndex = 0,
 
-          if (this.frameIndex === 1) {
-            this.frameIndex = 0;
-          } else {
-            this.frameIndex += 1;
-          }
-        }
-        this.frame = this.frameSet[this.frameIndex];
+    //for an animation use the change function to change to it and change the frameset to an array of animation. the animation may also need to be its own object.
+    this.change = function (frameSet, delay) {
+      if (this.frameSet != frameSet) {
+
+        this.count = 0;
+        this.delay = delay;
+        this.frameIndex = 0;
+        this.frameSet = frameSet;
+        this.frame = frameSet[this.frameIndex];
       }
+    },
+
+    this.update = function () {
+      this.count++;
+
+      if (this.count >= this.delay) {
+        this.count = 0;
+
+
+
+        if (this.frameIndex === 1) {
+          this.frameIndex = 0;
+        } else {
+          this.frameIndex += 1;
+        }
+      }
+      this.frame = this.frameSet[this.frameIndex];
+    }
+};
+
+var banditSpriteSheet = {
+  frameSet: [[0, 1]],
+  image: new Image()
+};
+
+
+banditSpriteSheet.image.src = "images/banditIdle32.png";
+
+var goodGuySpriteSheet = {
+  frameSet: [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]],
+  image: new Image()
+};
+
+
+goodGuySpriteSheet.image.src = "images/pallySheet.png";
+
+var banditIdle = {
+  animation: new CharAnimation(banditSpriteSheet.frameSet),
+  height: 32,
+  width: 32,
+  x: 150,
+  y: 165
+};
+
+var goodGuyIdle = {
+  animation: new CharAnimation(goodGuySpriteSheet.frameSet),
+  height: 32,
+  width: 32,
+  x: 85,
+  y: 165
+};
+
+
+
+
+var loop = function () {
+  // i think this is where we will put our big condition statement
+
+  goodGuyIdle.animation.change(goodGuySpriteSheet.frameSet[2], 40);
+  banditIdle.animation.change(banditSpriteSheet.frameSet[0], 20);
+  ctx.clearRect(0, 0, 480, 640)
+  ctx.drawImage(backgroundImg, 0, 0);
+
+  ctx.drawImage(goodGuySpriteSheet.image, goodGuyIdle.animation.frame * SPRITE_SIZE,
+    0, SPRITE_SIZE, SPRITE_SIZE, Math.floor(goodGuyIdle.x), Math.floor(goodGuyIdle.y), SPRITE_SIZE, SPRITE_SIZE);
+
+  ctx.drawImage(banditSpriteSheet.image, banditIdle.animation.frame * SPRITE_SIZE, 0,
+    SPRITE_SIZE, SPRITE_SIZE, Math.floor(banditIdle.x), Math.floor(banditIdle.y), SPRITE_SIZE, SPRITE_SIZE);
+
+  banditIdle.animation.update();
+  goodGuyIdle.animation.update();
+  window.requestAnimationFrame(loop);
+};
+
+
+
+
+
+
+function renderNewSprite(image, x, y) {
+  image.onload = function () {
+
+    ctx.drawImage(image, x, y);
   };
 
-  var banditSpriteSheet = {
-    frameSet: [[0, 1]],
-    image: new Image()
-  };
+}
 
+//the constructor for new sprites on the canvas
+var Asset = function (image, x, y, velocity) {
+  this.image = image;
+  this.x = x;
+  this.y = y;
+  this.velocity = velocity;
 
-  banditSpriteSheet.image.src = "images/banditIdle32.png";
+  renderNewSprite(image, x, y);
 
-  var goodGuySpriteSheet = {
-    frameSet: [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]],
-    image: new Image()
-  };
-
-
-  goodGuySpriteSheet.image.src = "images/pallySheet.png";
-
-  var banditIdle = {
-    animation: new CharAnimation(banditSpriteSheet.frameSet),
-    height: 32,
-    width: 32,
-    x: 150,
-    y: 165
-  };
-
-  var goodGuyIdle = {
-    animation: new CharAnimation(goodGuySpriteSheet.frameSet),
-    height: 32,
-    width: 32,
-    x: 85,
-    y: 165
-  };
+};
 
 
 
 
-  var loop = function () {
-    // i think this is where we will put our big condition statement
+// var spriteSheet = {
 
-    goodGuyIdle.animation.change(goodGuySpriteSheet.frameSet[2], 40);
-    banditIdle.animation.change(banditSpriteSheet.frameSet[0], 20);
-    ctx.clearRect(0, 0, 480, 640)
-    ctx.drawImage(backgroundImg, 0, 0);
-
-    ctx.drawImage(goodGuySpriteSheet.image, goodGuyIdle.animation.frame * SPRITE_SIZE,
-      0, SPRITE_SIZE, SPRITE_SIZE, Math.floor(goodGuyIdle.x), Math.floor(goodGuyIdle.y), SPRITE_SIZE, SPRITE_SIZE);
-
-    ctx.drawImage(banditSpriteSheet.image, banditIdle.animation.frame * SPRITE_SIZE, 0,
-      SPRITE_SIZE, SPRITE_SIZE, Math.floor(banditIdle.x), Math.floor(banditIdle.y), SPRITE_SIZE, SPRITE_SIZE);
-
-    banditIdle.animation.update();
-    goodGuyIdle.animation.update();
-    window.requestAnimationFrame(loop);
-  };
+//     frameSets: [[/* idle */],
+//     [/* gg Sword bg Sword */],
+//     [/* gg Sword bg Spell */],
+//     [/* gg Sword bg Shield */],
+//     [/* gg Spell bg Sword */],
+//     [/* gg Spell bg Spell */],
+//     [/* gg Spell bg S+hield */],
+//     [/* gg Shield bg Sword */],
+//     [/* gg Shield bg Spell */],
+//     [/* gg Shield bg Shield */],],
 
 
+// };
 
 
+goodGuySpriteSheet.image.addEventListener("load", function (event) {
 
-
-  function renderNewSprite(image, x, y) {
-    image.onload = function () {
-
-      ctx.drawImage(image, x, y);
-    };
-
-  }
-
-  //the constructor for new sprites on the canvas
-  var Asset = function (image, x, y, velocity) {
-    this.image = image;
-    this.x = x;
-    this.y = y;
-    this.velocity = velocity;
-
-    renderNewSprite(image, x, y);
-
-  };
-
-
-
-
-  // var spriteSheet = {
-
-  //     frameSets: [[/* idle */],
-  //     [/* gg Sword bg Sword */],
-  //     [/* gg Sword bg Spell */],
-  //     [/* gg Sword bg Shield */],
-  //     [/* gg Spell bg Sword */],
-  //     [/* gg Spell bg Spell */],
-  //     [/* gg Spell bg S+hield */],
-  //     [/* gg Shield bg Sword */],
-  //     [/* gg Shield bg Spell */],
-  //     [/* gg Shield bg Shield */],],
-
-
-  // };
-
-
-  goodGuySpriteSheet.image.addEventListener("load", function (event) {
-
-    window.requestAnimationFrame(loop);
-  });
+  window.requestAnimationFrame(loop);
+});
 
   // //character animation functions
   // function idle() {
