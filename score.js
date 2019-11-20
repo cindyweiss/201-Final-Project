442c89c9c1f305897feaa0c567de6d4e19b928af
Jerome Burns <jeromeb1238@hotmail.com>
2019-11-20 12:53:31 -0800

    re-added the header and center in css

--- score.js
+++ score.js
@@ -1,18 +1,35 @@
+// render header row; start with blank
+// function renderHours(domReference) {
+//     var tr = document.createElement('tr');
+//     var th = document.createElement('th');
+//     th.textContent = '';
+//     tr.appendChild(th);
+
+//     for (var storeHoursIndex = 0; storeHoursIndex < storeHours.length; storeHoursIndex++) {
+//         th = document.createElement('th');
+//         th.textContent = storeHours[storeHoursIndex];
+//         tr.append(th);
+//     }
+//     th = document.createElement('th');
+//     th.textContent = 'Totals';
+//     tr.appendChild(th);
+//     domReference.append(tr);
+// }
 
 
 
-var render = function () {
+var render = function() {
 
-  // var score = document.getElementById('score');
-  var tableContents = document.getElementById('scores');
-  var tr = document.createElement('tr');
-  var nameCell = document.createElement('td');
-  nameCell.textContent = this.name;
-  tr.append(nameCell);
+    // var score = document.getElementById('score');
+    var tableContents = document.getElementById('scores');
+    var tr = document.createElement('tr');
+    var nameCell = document.createElement('td');
+    nameCell.textContent = this.name;
+    tr.append(nameCell);
 
-  var matchCount = document.createElement('td');
-  matchCount.textContent = this.userHistory;
-  tr.append(matchCount);
+    var matchCount = document.createElement('td');
+    matchCount.textContent = this.userHistory;
+    tr.append(matchCount);
 
-  tableContents.append(tr);
+    tableContents.append(tr);
 };
