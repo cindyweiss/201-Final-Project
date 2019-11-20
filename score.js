4dca3a38389d83fa82ec5ecef1a60aafbe357b38
Jerome Burns <jeromeb1238@hotmail.com>
2019-11-20 15:03:35 -0800

    css edits

--- score.js
+++ score.js
@@ -5,64 +5,30 @@
 //     th.textContent = '';
 //     tr.appendChild(th);
 
-<<<<<<< HEAD
-//     for (var storeHoursIndex = 0; storeHoursIndex < storeHours.length; storeHoursIndex++) {
-//         th = document.createElement('th');
-//         th.textContent = storeHours[storeHoursIndex];
-//         tr.append(th);
-//     }
-//     th = document.createElement('th');
-//     th.textContent = 'Totals';
-//     tr.appendChild(th);
-//     domReference.append(tr);
-// }
+var SCORE_DATA = 'SCORE_DATA';
 
-
-
-var render = function() {
+var render = function(user) {
 
     // var score = document.getElementById('score');
     var tableContents = document.getElementById('scores');
     var tr = document.createElement('tr');
     var nameCell = document.createElement('td');
-    nameCell.textContent = this.name;
+    nameCell.textContent = user.name;
     tr.append(nameCell);
 
     var matchCount = document.createElement('td');
-    matchCount.textContent = this.userHistory;
+    matchCount.textContent = user.winLossHistory;
     tr.append(matchCount);
-
+    console.log(this);
     tableContents.append(tr);
-};
-=======
-var SCORE_DATA = 'SCORE_DATA';
-
-var render = function (user) {
-
-  // var score = document.getElementById('score');
-  var tableContents = document.getElementById('scores');
-  var tr = document.createElement('tr');
-  var nameCell = document.createElement('td');
-  nameCell.textContent = user.name;
-  tr.append(nameCell);
-
-  var matchCount = document.createElement('td');
-  matchCount.textContent = user.winLossHistory;
-  tr.append(matchCount);
-  console.log(this);
-  tableContents.append(tr);
 };
 
 
 var jsonData = localStorage.getItem(SCORE_DATA);
 var dataForHighScores = JSON.parse(jsonData);
 for (var i = 0; i < dataForHighScores.length; i++) {
-  render(dataForHighScores[i]);
+    render(dataForHighScores[i]);
 }
 
 
 console.log(dataForHighScores);
-
-
-
->>>>>>> 2f39c7d0f202d523588a4ad948bfce172c6d9578
